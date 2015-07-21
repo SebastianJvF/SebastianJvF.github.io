38989cc01b185468bc450feae7d3fdf4fbdc14e5
Patrick Sebastian John von Freyend <sebastian-jvf@live.de>
2015-07-21 23:44:11 +0200

    Checks now if button is placed inside the case

--- js/app/information-messages.js
+++ js/app/information-messages.js
@@ -5,6 +5,6 @@ var showMessageBlue = '#6088FF';
 function showMessage(message, backgroundColor) {
 	$("#info-message").html(message);
 	$("#info-message").css("background-color", backgroundColor);
-	$("#info-message").show().delay(5000).fadeOut();
+	$("#info-message").show().delay(7000).fadeOut();
 	$("#info-message").slideUp("normal");
 }
