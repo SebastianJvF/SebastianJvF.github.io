9815917a41e368c81f9977e221b83b439c159089
Patrick Sebastian John von Freyend <sebastian-jvf@live.de>
2015-08-09 13:19:08 +0200

    Test

--- js/app/create-two-d-phone-model.js
+++ js/app/create-two-d-phone-model.js
@@ -1,4 +1,4 @@
-function createTwoDPhoneModel(depth, width, height) {
+function createTwoDPhoneModel(depth, width, height, additionalObjects, additionalObjectsCount) {
 	canvas.clear();
 
 	var spacing = 18;
@@ -10,13 +10,6 @@ function createTwoDPhoneModel(depth, width, height) {
 	var leftFirst = ((canvasWidth)-width-2*depth-2*spacing)/2;
 	var leftSecond = leftFirst + depth + spacing;
 	var leftThird = leftSecond + width + spacing;
-	
-	var topCamera = topSecond + 15;
-	var leftCamera = leftSecond + 15;
-	
-	var modelStrokeWidth = 2;
-	var modelColor = '#ffffff';
-	var modelStrokeColor = '#c3bfbf';
 	
 	middleRect = new fabric.Rect({
 			top : topSecond,
@@ -68,21 +61,13 @@ function createTwoDPhoneModel(depth, width, height) {
 			stroke: modelStrokeColor
 	});
 	
-	cameraHole = new fabric.Circle({
-			top: topCamera,
-			left: leftCamera,
-			radius: 12,
-			fill : modelColor,
-			strokeWidth : modelStrokeWidth,
-			stroke: modelStrokeColor
-	});
-	
 	canvas.add(middleRect);
 	canvas.add(leftRect);
 	canvas.add(rightRect);
 	canvas.add(topRect);
 	canvas.add(bottomRect);
-	canvas.add(cameraHole);
+	
+	/* Add additional objects */
 		
 	/* Make mock-up unselectable */
 	canvas.item(0).selectable = false;
@@ -90,14 +75,12 @@ function createTwoDPhoneModel(depth, width, height) {
 	canvas.item(2).selectable = false;
 	canvas.item(3).selectable = false;
 	canvas.item(4).selectable = false;
-	canvas.item(5).selectable = false;
 	
 	canvas.item(0).setCoords();
 	canvas.item(1).setCoords();
 	canvas.item(2).setCoords();
 	canvas.item(3).setCoords();
 	canvas.item(4).setCoords();
-	canvas.item(5).setCoords();
 	
 	canvas.renderAll();
 }
