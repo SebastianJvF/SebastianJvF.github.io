8ad9236ea5f9b572f4610b5b66626fd83675bf6b
Patrick Sebastian John von Freyend <sebastian-jvf@live.de>
2015-08-11 15:39:00 +0200

    Placing multiple elements, placing non valid areas
    Placing non valid areas no works by using the arguments[]-functionality
    in javascript
    
    Placing multiple elements on the phone no works by setting the
    parseString in a for loop

--- js/app/add-draggable-objects.js
+++ js/app/add-draggable-objects.js
@@ -1,33 +1,33 @@
 /* DRAG AND DROP */
-/* Dragend - event $(document).bind('dragend', function(){ console.log("Dragend"); }); */ 
+/* Dragend - event $(document).bind('dragend', function(){ console.log("Dragend"); }); */
 /* Dragging workaround: set variable to true when clicked on link and check on mouse up */
 function createDraggableObject(fabricObject, buttonName) {
 	var middleRect, leftRect, rightRect, topRect, bottomRect, cameraHole;
 	var draggingStarted = false;
-	
+
 	/* Create a deep copy of the object to be able to add multiple elements */
 	var deepCopy =  $.extend({}, fabricObject);
-	
+
 	$('#' + buttonName).mousedown(function() {
 		draggingStarted = true;
-		
+
 		/* Change mouse cursor while dragging */
 		$("nav").css('cursor', 'copy');
 		$("div").css('cursor', 'copy');
 		$("a").css('cursor', 'copy');
 		canvas.defaultCursor = 'copy';
-		
+
 		$("#fire-dragend").mouseup(function(e) {
 			if(draggingStarted == true) {
 				console.log("Mouseup");
 				draggingStarted = false;
-				
+
 				/* Change mouse cursor back as we're done dropping */
 				$("nav").css('cursor', 'auto');
 				$("div").css('cursor', 'auto');
 				$("a").css('cursor', 'auto');
 				canvas.defaultCursor = 'auto';
-				
+
 				/* Disable scaling */
 				deepCopy.lockScalingX = deepCopy.lockScalingY = true;
 				deepCopy.setControlsVisibility({
@@ -41,23 +41,23 @@ function createDraggableObject(fabricObject, buttonName) {
 				    tr: false,
 				    mtr: true // Enable rotation
 				});
-				
+
 				/* Get mouse position on canvas */
 				var offset = $("#fire-dragend").offset();
 				var mouseCoordinates = {'x': e.pageX-offset.left, 'y': e.pageY-offset.top};
-				
-				/* Add object */	
+
+				/* Add object */
 				canvas.add(deepCopy);
 				showMessage("Added button", showMessageBlue);
-				
+
 				/* Set coordinates */
 				deepCopy.setTop(mouseCoordinates.y);
 				deepCopy.setLeft(mouseCoordinates.x);
-				
+
 				/* Let element be further moved */
 				deepCopy.setCoords();
 				canvas.renderAll();
-				
+
 				/* Check, on which side the button was placed */
 				if(deepCopy.isContainedWithinObject(canvas.item(0))) { deepCopy.contained = 0; }
 				else if(deepCopy.isContainedWithinObject(canvas.item(1))) { deepCopy.contained = 1; }
@@ -66,36 +66,37 @@ function createDraggableObject(fabricObject, buttonName) {
 				else if(deepCopy.isContainedWithinObject(canvas.item(4))) { deepCopy.contained = 4; }
 				else { deepCopy.contained = 6; }
 				deepCopy.buttonName = buttonName;
-				
+				deepCopy.correctlyPlaced = true;
+
 				// Trigger an event when loaded
 				$.event.trigger({
 					type: "modelChanged",
 					time: new Date()
 				});
-				
+
 				/* Create a new deep copy to empty the old one */
 				deepCopy =  $.extend({}, fabricObject);
 			}
 		});
-		
+
 		/* To end mousedown event right afterwards */
 		return false;
 	});
 }
 
 function addButtonAction(url, id) {
-	var group = [];	   
+	var group = [];
 	var loadedObjects;
-	 
+
 	fabric.loadSVGFromURL(
-	        	
-	        	url, 
-	        	
+
+	        	url,
+
 	        	function(objects, options) {
-	            	loadedObjects = new fabric.Group(group);	    
+	            	loadedObjects = new fabric.Group(group);
 	            	createDraggableObject(loadedObjects, id);
-	            }, 
-	            
+	            },
+
 	            function(item, object) {
 	                object.set('id', item.getAttribute('id'));
 	                group.push(object);
--- js/app/create-two-d-phone-model.js
+++ js/app/create-two-d-phone-model.js
@@ -1,16 +1,17 @@
-function createTwoDPhoneModel(depth, width, height, additionalObjects, additionalObjectsCount) {
+function createTwoDPhoneModel() {
 	canvas.clear();
 
+	var depth = arguments[0], width = arguments[1], height = arguments[2];
+
 	var spacing = 18;
-	
 	var topFirst = (canvasHeight-2*spacing-height-2*depth)/2;
 	var topSecond = topFirst+depth+spacing;
 	var topThird = topSecond+height+spacing;
-	
+
 	var leftFirst = ((canvasWidth)-width-2*depth-2*spacing)/2;
 	var leftSecond = leftFirst + depth + spacing;
 	var leftThird = leftSecond + width + spacing;
-	
+
 	middleRect = new fabric.Rect({
 			top : topSecond,
 			left : leftSecond,
@@ -20,7 +21,7 @@ function createTwoDPhoneModel(depth, width, height, additionalObjects, additiona
 			strokeWidth : modelStrokeWidth,
 			stroke: modelStrokeColor
 	});
-		
+
 	leftRect = new fabric.Rect({
 			top : topSecond,
 			left : leftFirst,
@@ -30,7 +31,7 @@ function createTwoDPhoneModel(depth, width, height, additionalObjects, additiona
 			strokeWidth : modelStrokeWidth,
 			stroke: modelStrokeColor
 	});
-	
+
 	rightRect = new fabric.Rect({
 			top : topSecond,
 			left : leftThird,
@@ -40,7 +41,7 @@ function createTwoDPhoneModel(depth, width, height, additionalObjects, additiona
 			strokeWidth : modelStrokeWidth,
 			stroke: modelStrokeColor
 	});
-		
+
 	topRect = new fabric.Rect({
 			top : topFirst,
 			left : leftSecond,
@@ -50,7 +51,7 @@ function createTwoDPhoneModel(depth, width, height, additionalObjects, additiona
 			strokeWidth : modelStrokeWidth,
 			stroke: modelStrokeColor
 	});
-		
+
 	bottomRect = new fabric.Rect({
 			top : topThird,
 			left : leftSecond,
@@ -60,27 +61,38 @@ function createTwoDPhoneModel(depth, width, height, additionalObjects, additiona
 			strokeWidth : modelStrokeWidth,
 			stroke: modelStrokeColor
 	});
-	
+
 	canvas.add(middleRect);
 	canvas.add(leftRect);
 	canvas.add(rightRect);
 	canvas.add(topRect);
 	canvas.add(bottomRect);
-	
+
+	var element;
+
 	/* Add additional objects */
-		
+	for(var i = 3; i < arguments.length; i++) {
+			canvas.add(arguments[i]);
+	}
+
 	/* Make mock-up unselectable */
 	canvas.item(0).selectable = false;
 	canvas.item(1).selectable = false;
 	canvas.item(2).selectable = false;
 	canvas.item(3).selectable = false;
 	canvas.item(4).selectable = false;
-	
+	for(var i = 5; i < arguments.length+5-3; i++) {
+			canvas.item(i).selectable = false;
+	}
+
 	canvas.item(0).setCoords();
 	canvas.item(1).setCoords();
 	canvas.item(2).setCoords();
 	canvas.item(3).setCoords();
 	canvas.item(4).setCoords();
-	
+	for(var i = 5; i < arguments.length+5-3; i++) {
+			canvas.item(i).setCoords();
+	}
+
 	canvas.renderAll();
 }
